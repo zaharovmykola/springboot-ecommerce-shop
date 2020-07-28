b187bdd1b3e86a30ad56aa6bddfa97a57cbe77d4
zaharovmykola <zaharovmykola@gmail.com>
2020-07-28 14:32:36 +0200

    CategoryControllerMethodsTest class works

---
+++ src/test/java/org/mykola/zakharov/spring/boot/first/ecommerceshop/application/controller/CategoryControllerMethodsTest.java
@@ -0,0 +1,67 @@
+package org.mykola.zakharov.spring.boot.first.ecommerceshop.application.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mykola.zakharov.spring.boot.first.ecommerceshop.controller.CategoryController;
+import org.mykola.zakharov.spring.boot.first.ecommerceshop.model.CategoryModel;
+import org.mykola.zakharov.spring.boot.first.ecommerceshop.model.ResponseModel;
+import org.mykola.zakharov.spring.boot.first.ecommerceshop.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class CategoryControllerMethodsTest {
+
+    @Autowired
+    private CategoryController categoryController;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Test
+    public void shouldReturnAllCategories () {
+        categoryController.getAll();
+        ResponseEntity responseEntity = categoryController.getAll();
+        assertNotNull(responseEntity);
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldCreateCategory () {
+        CategoryModel categoryModel
+                = CategoryModel.builder()
+                .name("test category 1").build();
+        ResponseEntity responseEntity = categoryController.create(categoryModel);
+        assertNotNull(responseEntity);
+        assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
+    }
+
+    @Test
+    public void shouldUpdateCategory () {
+        CategoryModel categoryModel
+                = CategoryModel.builder()
+                .id(1L).name("test category 2").build();
+        categoryController.create(categoryModel);
+        ResponseEntity responseEntityUpdate
+                = categoryController.update(categoryModel.getId(), categoryModel);
+        assertNotNull(responseEntityUpdate);
+        assertEquals(responseEntityUpdate.getStatusCode(), HttpStatus.OK);
+    }
+
+    @Test
+    public void shouldDeleteCategory () {
+        CategoryModel categoryModel
+                = CategoryModel.builder()
+                .id(2L).name("test category 3").build();
+        categoryController.create(categoryModel);
+        ResponseEntity responseEntityDelete
+                = categoryController.deleteCategory(categoryModel.getId());
+        assertNotNull(responseEntityDelete);
+        assertEquals(responseEntityDelete.getStatusCode(), HttpStatus.OK);
+    }
+
+}
