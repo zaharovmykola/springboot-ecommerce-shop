7a9429919ea8dac404041f086e3961206ccc4e70
zaharovmykola <zaharovmykola@gmail.com>
2020-08-03 10:44:17 +0200

    + one more singout test do not work

--- src/test/java/org/mykola/zakharov/spring/boot/first/ecommerceshop/ui/SignOutPageTest.java
+++ src/test/java/org/mykola/zakharov/spring/boot/first/ecommerceshop/ui/SignOutPageTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mykola.zakharov.spring.boot.first.ecommerceshop.EcommerceShopApplication;
 import org.mykola.zakharov.spring.boot.first.ecommerceshop.ui.pagefactory.SignInPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,9 @@ import static org.junit.jupiter.api.Assertions.*;
         classes = EcommerceShopApplication.class
 )
 public class SignOutPageTest extends AbstractPageTest {
+
+    private By signOutButton = By.cssSelector("nav a[href='#!home:out']");
+    private By logOutButton = By.cssSelector("nav a[href*='#!home:out']");
 
     private SignInPage signInPage;
 
@@ -58,6 +65,23 @@ public class SignOutPageTest extends AbstractPageTest {
         assertEquals("http://localhost:8090/eCommerceShop/#!home", driver.getCurrentUrl());
         String logOutButtonTextAfterSignOut = indexPage.getLogOutButtonText();
         assertEquals("", logOutButtonTextAfterSignOut);
+    }
+
+    @Test
+    @Order(3)
+    public void checkingSignOutStyleAndTextAvailability() throws InterruptedException {
+        signInPage.loginWithValidCredentials("one", "UserPassword1");
+        Thread.sleep(5000);
+        indexPage.getLogOutButtonText();
+        List<WebElement> logOutButtonElementAfterLogIn =
+                driver.findElements(logOutButton);
+        assertTrue(logOutButtonElementAfterLogIn.contains("display: block;"));
+
+        signInPage.loginOut("one", "UserPassword1");
+        Thread.sleep(5000);
+        List<WebElement> logOutButtonElementAfterLogOut =
+                driver.findElements(logOutButton);
+        assertTrue(logOutButtonElementAfterLogOut.contains("display: none;"));
     }
 
 }
